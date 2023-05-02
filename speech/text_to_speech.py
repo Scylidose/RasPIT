a9ba15dee4e12daad8f5ce945965e8bfcdfc85c3
Scylidose <jordisaleilles@hotmail.fr>
2023-05-01 22:09:26 -0400

    Synthesize the speech from a text to an audio in french female voice.

---
+++ speech/text_to_speech.py
@@ -0,0 +1,21 @@
+import os
+from google.cloud import texttospeech
+from config import GOOGLE_APPLICATION_CREDENTIALS
+
+def synthesize_speech(text, language_code="fr-FR"):
+    client = texttospeech.TextToSpeechClient()
+    input_text = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=language_code,
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input=input_text, voice=voice, audio_config=audio_config
+    )
+
+    return response.audio_content
