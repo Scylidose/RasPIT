7e6508ea0660bc76f758a92eaf1afc2a9c95fa1e
Scylidose <jordisaleilles@hotmail.fr>
2023-05-07 23:21:25 -0400

    Changed code implementation from pyaudio to sounddevice.

--- wake_word.py
+++ wake_word.py
@@ -9,49 +9,26 @@ porcupine_path = os.getenv("PATH_TO_PORCUPINE")
 sys.path.append(porcupine_path)
 
 import pvporcupine
-import pyaudio
 import struct
 import io
 import wave
 import requests
-from pygame import mixer
+
+import sounddevice as sd
+import numpy as np
 
 def process_audio_after_wake_word():
-    # Set up the audio recording parameters
     RECORD_SECONDS = 4
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
     RATE = 16000
-    CHUNK = 1024
 
-    # Set up PyAudio for recording
-    audio = pyaudio.PyAudio()
-
-    # Start recording
     print("Recording...")
-    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-    frames = []
-
-    for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    # Stop recording
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    # Save the recorded audio as a WAV file in memory
+    recording = sd.rec(int(RECORD_SECONDS * RATE), samplerate=RATE, channels=1, dtype=np.int16)
+    sd.wait()
+    
     wav_file = io.BytesIO()
-    with wave.open(wav_file, 'wb') as wf:
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(audio.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-
+    wavio.write(wav_file, recording, RATE, sampwidth=2)
     wav_file.seek(0)
 
-    # Send the audio file to the Flask app
     print("Sending audio to the server...")
     response = requests.post('http://localhost:5000/process_audio', files={'audio_file': wav_file})
 
@@ -60,14 +37,11 @@ def process_audio_after_wake_word():
         response_text = result['response_text']
         response_audio = result['response_audio']
 
-        # Download the response audio
         audio_data = requests.get(response_audio).content
 
-        # Save the response audio as a temporary file
         with open('temp_response.mp3', 'wb') as f:
             f.write(audio_data)
 
-        # Play the response audio using pygame.mixer
         mixer.init()
         mixer.music.load('temp_response.mp3')
         mixer.music.play()
@@ -82,40 +56,35 @@ def process_audio_after_wake_word():
 
 def main():
     porcupine = None
-    pa = None
     audio_stream = None
 
     try:
         porcupine = pvporcupine.create(keywords=["raspit"])
-        pa = pyaudio.PyAudio()
-        audio_stream = pa.open(
-            rate=porcupine.sample_rate,
-            channels=1,
-            format=pyaudio.paInt16,
-            input=True,
-            frames_per_buffer=porcupine.frame_length,
-            input_device_index=None,
-        )
-
-        while True:
-            pcm = audio_stream.read(porcupine.frame_length)
-            pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+        
+        def callback(indata, frames, time, status):
+            if status:
+                print("Error:", status)
+            pcm = indata.flatten()
             keyword_index = porcupine.process(pcm)
 
             if keyword_index >= 0:
                 print("Wake word detected")
                 process_audio_after_wake_word()
 
+        with sd.InputStream(
+            samplerate=porcupine.sample_rate,
+            channels=1,
+            dtype=np.int16,
+            blocksize=porcupine.frame_length,
+            callback=callback
+        ):
+            print("Listening for wake word...")
+            sd.sleep(-1)
+
     except KeyboardInterrupt:
         print("Stopping")
 
     finally:
-        if audio_stream is not None:
-            audio_stream.close()
-
-        if pa is not None:
-            pa.terminate()
-
         if porcupine is not None:
             porcupine.delete()
 
